6f96de571d67efc1a26a6abfb1892f1c1eb478aa
sodie-oxige <sodie.oxige@gmail.com>
2024-09-06 13:13:43 +0900

    feat: ✨起動表示の追加、ブランド名「PORT」の仮決定、日付の曜日表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def first_visit?
+    referer = request.referer
+    if referer.nil?
+      true
+    else
+      referer_uri = URI.parse(referer)
+      referer_host_with_port = "#{referer_uri.host}:#{referer_uri.port}"
+      current_host_with_port = request.host_with_port
+      referer_host_with_port != current_host_with_port
+    end
+  end
 end
