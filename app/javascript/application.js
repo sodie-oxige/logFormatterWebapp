6881e5d4601eda323a183a32f4d854a142bf98e0
sodie-oxige <sodie.oxige@gmail.com>
2024-09-09 00:35:02 +0900

    refactor: ♻️グローバル変数用scssファイル作成、css微調整、サイドバー切替ボタンがturbo遷移で動かなくなるバグ修正、栞が１進バグ修正、ログにてページタイトルをシナリオ名に

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,14 +4,15 @@ import "controllers"
 
 document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", e => {
-        let elem;
-        if (e.target.tagName == "TR") if (e.target.dataset.url) elem = e.target;
-        if (e.target.tagName == "TD") if (e.target.parentElement.dataset.url) elem = e.target.parentElement;
-        if (elem) location.href = elem.dataset.url;
+        if (e.target.tagName == "TR") if (e.target.dataset.url) location.href = e.target.dataset.url;
+        if (e.target.tagName == "TD") if (e.target.parentElement.dataset.url) location.href = e.target.parentElement.dataset.url;
+        if (e.target.classList.contains("nav_menubutton")){
+            document.getElementById("sidebar").classList.toggle("open");
+            e.target.classList.toggle("open");
+        }
+        if (e.target.parentElement.classList.contains("nav_menubutton")){
+            document.getElementById("sidebar").classList.toggle("open");
+            e.target.parentElement.classList.toggle("open");
+        }
     })
-})
-
-document.querySelector(".nav_menubutton").addEventListener("click", (e) => {
-    document.getElementById("sidebar").classList.toggle("open");
-    e.currentTarget.classList.toggle("open");
 })
