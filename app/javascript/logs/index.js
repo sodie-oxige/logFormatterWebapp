5abc2df19fb145178bede3e6108b9224cafd8882
sodie-oxige <sodie.oxige@gmail.com>
2024-09-15 20:27:33 +0900

    fix: 🐛turbo遷移でjsが発火しない不具合修正

--- app/javascript/logs/index.js
+++ app/javascript/logs/index.js
@@ -1,9 +1,11 @@
-document.getElementById("main").addEventListener("dragover", e => { e.preventDefault(); })
-document.getElementById("main").addEventListener("dragenter", e => { e.preventDefault(); })
-document.getElementById("main").addEventListener("dragleave", e => { e.preventDefault(); })
-document.getElementById("main").addEventListener("drop", e => {
-    e.preventDefault();
-    const dropFileForm = document.getElementById("dropFileForm");
-    dropFileForm.querySelector("input[type='file']").files = e.dataTransfer.files;
-    dropFileForm.submit();
+document.addEventListener("turbo:load", ()=>{
+    document.getElementById("main").addEventListener("dragover", e => { e.preventDefault(); })
+    document.getElementById("main").addEventListener("dragenter", e => { e.preventDefault(); })
+    document.getElementById("main").addEventListener("dragleave", e => { e.preventDefault(); })
+    document.getElementById("main").addEventListener("drop", e => {
+        e.preventDefault();
+        const dropFileForm = document.getElementById("dropFileForm");
+        dropFileForm.querySelector("input[type='file']").files = e.dataTransfer.files;
+        dropFileForm.submit();
+    })
 })
