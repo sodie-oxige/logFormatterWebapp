d380739e41007951f67b093ec5977fd3ff235e42
sodie-oxige <sodie.oxige@gmail.com>
2024-10-06 22:50:55 +0900

    refactor: ♻️ログ閲覧でGMのPCもNPC側表示に

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -67,7 +67,7 @@ class LogsController < ApplicationController
     @log_content = log.log_contents.find_by(index: @page)
     char = nil
     author = @log_content[:author].gsub(/[[:space:]]/, "")
-    char = Character.find_by("REGEXP_REPLACE(name, '\s+', '') = ? and ((is_pc = true and pl_id <> ?) or (is_pc = false and pl_id = ?))", author, log[:gm_id], log[:gm_id])
+    char = Character.find_by("REGEXP_REPLACE(name, '\s+', '') = ?", author)
     char ||= Nickname.find_by("REGEXP_REPLACE(name, '\s+', '') = ?", author)&.character
     if @log_content.present?
       log.update(bookmark: @page)
