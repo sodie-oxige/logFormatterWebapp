9094e2eb3a27ac00a9b9d75813ba252fb2d2856a
sodie-oxige <sodie.oxige@gmail.com>
2024-10-16 23:32:32 +0900

    feat: ✨footerメニューバーを実装、pwaで画面下部に余白ができるのを修正（dvh/w→vh/w）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   include SessionsHelper
   before_action :autheniticate_user
+
+  def page404
+    render template: "errors/404", status: 404
+  end
 end
