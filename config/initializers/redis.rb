4adf6b5088864aceb708154af653e700c63f0355
sodie-oxige <sodie.oxige@gmail.com>
2024-11-15 01:14:31 +0900

    fix: 🐛Upstashに接続できないバグ修正

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,7 @@ redis = Rails.application.config.x.redis
 $redis = Redis.new(
   host: redis[:host],
   port: redis[:port],
-  db: redis[:db]
+  password: redis[:password],
+  db: redis[:db],
+  ssl: true
 )
